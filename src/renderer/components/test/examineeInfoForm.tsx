5d34b445ac0491ed21bd3b7a43bb63a741cb1909
ktyoung <ktyoung0507@gmail.com>
2023-06-17 15:38:13 +0900

    Design InfoForm ,Refactor

--- src/renderer/components/test/examineeInfoForm.tsx
+++ src/renderer/components/test/examineeInfoForm.tsx
@@ -1,56 +1,66 @@
-import ico_refresh from '@assets/images/icons/icon_refresh.png';
-import ico_check from '@assets/images/icons/icon_check.png';
+import ico_refresh from '@assets/images/icons/icon_refresh_white.png';
+import ico_check from '@assets/images/icons/icon_check_white.png';
+import ico_speaker from '@assets/images/icons/icon_speaker.png';
 
 export default function ExamineeInfoForm() {
   return (
     <>
-      <h1 className="text-slate-950 text-center mb-10">
-        피검사자 기본 정보를 입력해 주세요.
-      </h1>
+      <div className="info-form-title">
+        <img src={ico_speaker} alt="speaker icon" />
+        <p>피검사자 기본 정보를 입력해 주세요.</p>
+      </div>
 
-      <div className="input-wrapper text-slate-950">
-        <ul className="flex flex-col">
-          <li className="flex justify-between items-center mb-5">
-            <p className="w-48">이름</p>
+      <div className="info-input-wrapper">
+        <ul className="info-input-inner">
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">01</p>
+            <p className="info-input-item-subject">이름</p>
             <input
               type="text"
               name="username"
               id="username"
-              className="border-b border-gray-300 focus:border-blue-500 w-full"
+              className="info-input-item-input"
             />
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <p className="w-48">성별</p>
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">02</p>
+            <p className="info-input-item-subject">성별</p>
             <input
               type="text"
-              name="gender"
-              id="gender"
-              className="border-b border-gray-300 focus:border-blue-500 w-full"
+              name="sex"
+              id="sex"
+              className="info-input-item-input"
             />
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <p className="w-48">생년월일</p>
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">03</p>
+            <p className="info-input-item-subject">생년월일</p>
             <input
               type="text"
               name="birthDate"
               id="birthDate"
-              className="border-b border-gray-300 focus:border-blue-500 w-full"
+              className="info-input-item-input"
             />
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <p className="w-48">피검사자번호</p>
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">04</p>
+            <p className="info-input-item-subject">피검사자번호</p>
             <input
               type="text"
               name="examineeId"
               id="examineeId"
-              className="border-b border-gray-300 focus:border-blue-500 w-full"
+              className="info-input-item-input"
             />
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <label htmlFor="direction-select" className="w-48">
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">05</p>
+            <label
+              htmlFor="direction-select"
+              className="info-input-item-subject"
+            >
               제시방향
             </label>
-            <select id="direction-select" className="w-full">
+            <select id="direction-select" className="info-input-item-input">
               <option value="좌">좌</option>
               <option value="우">우</option>
               <option value="양방향">양방향</option>
@@ -58,11 +68,12 @@ export default function ExamineeInfoForm() {
               <option value="우 노이즈 좌 스피치">우 노이즈 좌 스피치</option>
             </select>
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <label htmlFor="start-level" className="w-48">
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">06</p>
+            <label htmlFor="start-level" className="info-input-item-subject">
               시작레벨
             </label>
-            <select id="start-level" className="w-full">
+            <select id="start-level" className="info-input-item-input">
               <option value="-6">-6</option>
               <option value="-5">-5</option>
               <option value="-4">-4</option>
@@ -82,20 +93,22 @@ export default function ExamineeInfoForm() {
               <option value="+10">+10</option>
             </select>
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <label htmlFor="scoring-method" className="w-48">
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">07</p>
+            <label htmlFor="scoring-method" className="info-input-item-subject">
               채점방식
             </label>
-            <select id="scoring-method" className="w-full">
+            <select id="scoring-method" className="info-input-item-input">
               <option value="Digit Scoring">Digit Scoring</option>
               <option value="Tripet Scoring">Tripet Scoring</option>
             </select>
           </li>
-          <li className="flex justify-between items-center mb-5">
-            <label htmlFor="sound-set" className="w-48">
+          <li className="info-input-item">
+            <p className="info-input-item-order-number">08</p>
+            <label htmlFor="sound-set" className="info-input-item-subject">
               사운드세트
             </label>
-            <select id="sound-set" className="w-full">
+            <select id="sound-set" className="info-input-item-input">
               <option value="List 1">List 1</option>
               <option value="List 2">List 2</option>
               <option value="List 3">List 3</option>
@@ -104,28 +117,24 @@ export default function ExamineeInfoForm() {
               <option value="List 6">List 6</option>
             </select>
           </li>
-          <li className="flex flex-col flex-start mb-5">
-            <p className="w-48">참고사항</p>
-            <textarea
-              className="w-full h-32 border border-gray-300 shadow-md p-2 resize-none"
-              placeholder="메모를 입력하세요."
-            />
-          </li>
-          <div className="refresh-check-wrapper flex justify-between items-center">
-            <a
-              href="#!"
-              className="flex justify-center items-center rounded-full bg-gray-300 w-20 h-20 mx-auto"
-            >
-              <img src={ico_refresh} alt="ico_refresh" className="w-1/2" />
-            </a>
-            <a
-              href="#!"
-              className="flex justify-center items-center rounded-full bg-gray-300 w-20 h-20 mx-auto"
-            >
-              <img src={ico_check} alt="ico_check" className="w-1/2" />
-            </a>
-          </div>
         </ul>
+      </div>
+
+      <div className="info-memo-wraper">
+        <input
+          type="text"
+          className="info-memo-input"
+          placeholder="참고사항을 입력해주세요."
+        />
+      </div>
+
+      <div className="info-btn-wrapper">
+        <button className="info-btn" type="reset">
+          <img src={ico_refresh} alt="ico_refresh" />
+        </button>
+        <button className="info-btn" type="button">
+          <img src={ico_check} alt="ico_check" />
+        </button>
       </div>
     </>
   );
--- src/renderer/components/test/testForm.tsx
+++ src/renderer/components/test/testForm.tsx
@@ -12,58 +12,56 @@ export default function TestForm() {
   let [currentPage, setCurrentPage] = useState(0);
 
   return (
-    <div className="flex flex-col h-full p-5">
-      <div className="progress-bar flex justify-center items-center mb-10">
-        {[0, 1, 2, 3].map(function (a, i) {
-          return (
-            <div
-              key={i}
-              className={
-                currentPage === i
-                  ? 'w-4 h-4 rounded-full bg-gray-300 mr-3 active'
-                  : 'w-4 h-4 rounded-full bg-gray-300 mr-3'
-              }
-            >
-              <p className="hidden">진행 단계 표시</p>
-            </div>
-          );
-        })}
-      </div>
+    <div className="test-form-wrapper">
+      <div className="test-form-inner">
+        <div className="progress-bar">
+          {[0, 1, 2, 3].map(function (a, i) {
+            return (
+              <div key={i} className={currentPage === i ? 'dot active' : 'dot'}>
+                <p className="hidden">진행 단계 표시</p>
+              </div>
+            );
+          })}
+        </div>
 
-      <div className="progress-btn-wrapper flex justify-center items-center relative w-full">
-        <button
-          disabled={currentPage === 0 ? true : false}
-          type="button"
-          className="absolute left-0 w-1/6 disabled:opacity-10"
-          onClick={() => {
-            setCurrentPage(--currentPage);
-          }}
-        >
-          <img
-            src={ico_angle_left}
-            alt="icon_angle_left"
-            className="w-full h-full "
-          />
-        </button>
-        <button
-          disabled={currentPage === 3 ? true : false}
-          type="button"
-          className="absolute right-0 w-1/6 disabled:opacity-10"
-          onClick={() => {
-            setCurrentPage(++currentPage);
-          }}
-        >
-          <img
-            src={ico_angle_right}
-            alt="icon_angle_left"
-            className="w-full h-full"
-          />
-        </button>
+        <div className="progress-btn-wrapper">
+          <button
+            disabled={currentPage === 0 ? true : false}
+            type="button"
+            className="progress-btn prev-btn"
+            onClick={() => {
+              setCurrentPage(--currentPage);
+            }}
+          >
+            <img
+              src={ico_angle_left}
+              alt="icon_angle_left"
+              className="icon-arrow"
+            />
+          </button>
+          <button
+            disabled={currentPage === 3 ? true : false}
+            type="button"
+            className="progress-btn next-btn"
+            onClick={() => {
+              setCurrentPage(++currentPage);
+            }}
+          >
+            <img
+              src={ico_angle_right}
+              alt="icon_angle_left"
+              className="icon-arrow"
+            />
+          </button>
+        </div>
+
+        <div className="test-contents-wrapper">
+          {currentPage === 0 && <ExamineeInfoForm />}
+          {currentPage === 1 && <PreCheckScreen />}
+          {currentPage === 2 && <CheckScreen />}
+          {currentPage === 3 && <TestResult />}
+        </div>
       </div>
-      {currentPage === 0 && <ExamineeInfoForm />}
-      {currentPage === 1 && <PreCheckScreen />}
-      {currentPage === 2 && <CheckScreen />}
-      {currentPage === 3 && <TestResult />}
     </div>
   );
 }
