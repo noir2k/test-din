0633b04952b3f860eb38df98f11f25ab3d6ad673
noir2k <noir2k@gmail.com>
2023-06-20 12:34:58 +0900

    fix text

--- src/renderer/components/main/notice.tsx
+++ src/renderer/components/main/notice.tsx
@@ -7,7 +7,7 @@ export default function Notice() {
         <img width="30" src={ico_speaker} alt="speaker icon" />
         <p className="notice-text">
           본 검사는 XXX 청각 테스트 검사입니다. <br />
-          검사 전에 윈도우 알림림 등 다른 사운드를 발생시키는 <br />
+          검사 전에 윈도우 알림 등 다른 사운드를 발생시키는 <br />
           서비스를 종료해 주세요. <br />
           검사 전에 기기 사운드 볼륨을 적당한 수준으로 조정해 주세요.
         </p>
